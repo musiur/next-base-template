f563780681facc57fcef174e3819c31b06456421
musiur <musiur.opu@gmail.com>
2023-06-02 04:03:16 +0600

    comp-dev: Navigation: Responsiveness checked from tab to computer view

--- components/libs/core/Navigation.tsx
+++ components/libs/core/Navigation.tsx
@@ -34,10 +34,11 @@ enum STYLES {
   NAV = "sticky top-0 backdrop-blur-xl shadow py-2",
   CONTAINER = "container flex items-center justify-between gap-5 mx-auto",
   BRAND = "text-xl lg:text-2xl font-bold",
-  ITEMS = "flex items-center justify-center",
+  ITEMS = "hidden md:flex items-center justify-center",
   ITEM = "py-2 px-4 font-medium hover:bg-gray-100 rounded-lg cursor-pointer transition ease-in-out duration-300",
-  ACTIONS = "flex items-center justify-end gap-5",
-  CART = "relative h-full mr-5 flex items-center justify-center"
+  ACTIONS = "flex items-center justify-end gap-3 md:gap-5",
+  CART = "relative h-full mr-3 md:mr-5 flex items-center justify-center",
+  CART_ICON = "icon-base"
 }
 
 const Navigation = () => {
@@ -60,7 +61,7 @@ const Navigation = () => {
         <div className={STYLES.ACTIONS}>
           <Link href="/" className={STYLES.CART}>
             <Badge count={999}/>
-            <FontAwesomeIcon icon={faCartShopping} className="icon-base" />
+            <FontAwesomeIcon icon={faCartShopping} className={STYLES.CART_ICON} />
           </Link>
           <button className="btn-primary">Account</button>
         </div>
