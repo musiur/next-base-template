dbe3515b653bb3a605f8974c42a60ccc0c0b589d
musiur <musiur.opu@gmail.com>
2023-06-01 06:46:18 +0600

    button styled in home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ const Home = () => {
   const {toast, setToast} = useContext<UniToastType>(ToasterContext)
   console.log(toast)
   return (
-    <div>
+    <div className="p-5">
       <h1>Home Page</h1>
-      <button onClick={() => setToast({show: true, type: true, text: "This is a test to show!"})}>Set Toast</button>
+      <button onClick={() => setToast({show: true, type: true, text: "This is a test to show!"})} className="m-5 px-4 py-[0.5rem] rounded-lg border border-gray-800 hover:bg-gray-800 hover:text-white transition ease-in-out duration-150">Set Toast</button>
     </div>
   );
 };
