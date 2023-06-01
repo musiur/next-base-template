89d789e797983ab0f26a39234771b5f157648943
musiur <musiur.opu@gmail.com>
2023-06-02 02:53:20 +0600

    comp-dev: Navigation: brand, items, action buttton added; styling: Navigation: NAV, CONTAINER, BRAND, ITEMS, ITEM, ACTION named enum added to style all the JSX element

--- components/libs/core/Navigation.tsx
+++ components/libs/core/Navigation.tsx
@@ -1,7 +1,65 @@
-const Navigation = () => {
-    return (
-        <nav></nav>
-    )
+"use client";
+
+import Link from "next/link";
+
+interface NavigationItemType {
+  id: number;
+  text: string;
+  link: string;
 }
+
+const NavigationItems: NavigationItemType[] = [
+  {
+    id: 0,
+    text: "About",
+    link: "/about",
+  },
+  {
+    id: 1,
+    text: "Contact",
+    link: "/contact",
+  },
+  {
+    id: 2,
+    text: "Item",
+    link: "/item",
+  },
+];
+
+
+// styles
+enum STYLES {
+  NAV = "sticky top-0 backdrop-blur-xl",
+  CONTAINER = "container flex items-center justify-between gap-5 mx-auto my-1",
+  BRAND = "text-xl lg:text-2xl font-bold",
+  ITEMS = "flex items-center justify-center",
+  ITEM = "py-2 px-4 font-medium hover:bg-gray-100 rounded-lg cursor-pointer transition ease-in-out duration-300",
+  ACTIONS = "flex items-center justify-end gap-3",
+}
+
+const Navigation = () => {
+  return (
+    <nav className={STYLES.NAV}>
+      <div className={STYLES.CONTAINER}>
+        <div className={STYLES.BRAND}>
+          <Link href="/">Brand</Link>
+        </div>
+        <ul className={STYLES.ITEMS}>
+          {NavigationItems.map((item: NavigationItemType) => {
+            const { id, text, link } = item;
+            return (
+              <li key={id} className={STYLES.ITEM}>
+                <Link href={link}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={STYLES.ACTIONS}>
+          <button>Account</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
