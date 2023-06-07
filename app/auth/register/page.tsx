e8dbe50ad1617882d714bf90067a0fc2353ee8b0
Musiur <musiur.opu@gmail.com>
2023-06-07 17:34:03 +0600

    comp-dev:: register & login: form validation and password icon state management

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Register = () => {
@@ -11,6 +14,7 @@ const Register = () => {
     confirm_password: "",
   });
   const [errors, setErrors] = useState(formData);
+  const [showPass, setShowPass] = useState(false);
 
   const handleOnChange = (e: any) => {
     const { name, value } = e.target;
@@ -60,35 +64,69 @@ const Register = () => {
     }
   }, [errors]);
   return (
-    <div className="container border">
-      <div className="max-w-[400px] mx-auto my-10 rounded-xl p-3 lg:p-5 shadow-xl">
+    <div className="container mx-auto">
+      <div className="max-w-[400px] mx-auto my-10 rounded-xl p-3 lg:p-5 border shadow">
         <h6>Register</h6>
         <form>
           <div>
             <label htmlFor="username">Username</label>
-            <input type="text" onChange={handleOnChange} id="username" />
+            <input
+              type="text"
+              name="username"
+              onChange={handleOnChange}
+              id="username"
+            />
             {errors.username ? <span>{errors.username}</span> : null}
           </div>
           <div>
             <label htmlFor="email">email</label>
-            <input type="text" onChange={handleOnChange} id="email" />
+            <input
+              type="text"
+              name="email"
+              onChange={handleOnChange}
+              id="email"
+            />
             {errors.email ? <span>{errors.email}</span> : null}
           </div>
           <div>
-            <label htmlFor="password">password</label>
-            <input type="text" onChange={handleOnChange} id="password" />
+            <label htmlFor="password">Password</label>
+            <input
+              type={showPass ? "text" : "password"}
+              name="password"
+              onChange={handleOnChange}
+              id="password"
+            />
             {errors.password ? <span>{errors.password}</span> : null}
+            <FontAwesomeIcon
+              icon={!showPass ? faEyeSlash : faEye}
+              onClick={() => setShowPass(!showPass)}
+              className="absolute top-[36px] right-0 mr-1 text-gray-300 hover:text-gray-600 icon cursor-pointer"
+            />
           </div>
           <div>
             <label htmlFor="confirm_password">Confirm password</label>
             <input
-              type="text"
+              type={showPass ? "text" : "password"}
+              name="confirm_password"
               onChange={handleOnChange}
               id="confirm_password"
             />
             {errors.confirm_password ? (
               <span>{errors.confirm_password}</span>
             ) : null}
+            <FontAwesomeIcon
+              icon={!showPass ? faEyeSlash : faEye}
+              onClick={() => setShowPass(!showPass)}
+              className="absolute top-[36px] right-0 mr-1 text-gray-300 hover:text-gray-600 icon cursor-pointer"
+            />
+          </div>
+          <button className="btn-primary w-full" onClick={handleOnSubmit}>
+            Register
+          </button>
+          <div>
+            <Link href="/auth/login" className="text-center">
+              Already have account? Login
+            </Link>
           </div>
         </form>
       </div>
